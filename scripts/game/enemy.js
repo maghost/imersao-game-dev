780770b0af226ebbc627bfd7a0bcad9384240a9b
Matheus Maghost <matheus@gamersclub.com.br>
2020-06-24 20:20:07 -0300

    feat: move enemy

--- scripts/game/enemy.js
+++ scripts/game/enemy.js
@@ -1,5 +1,12 @@
 class Enemy extends SpriteAnimation {
   constructor(props) {
     super(props);
+
+    this.positionX = props.positionX;
+    this.velocity = props.velocity || 10;
+  }
+
+  move() {
+    this.positionX = this.positionX - this.velocity;
   }
 }
