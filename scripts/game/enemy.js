ed6c067cb7cf85ae15d4718033eaa161af826028
Matheus Maghost <matheus@gamersclub.com.br>
2020-06-24 20:36:14 -0300

    feat: add move responsability to sprite animation

--- scripts/game/enemy.js
+++ scripts/game/enemy.js
@@ -1,12 +1,5 @@
 class Enemy extends SpriteAnimation {
   constructor(props) {
     super(props);
-
-    this.positionX = props.positionX;
-    this.velocity = props.velocity || 10;
-  }
-
-  move() {
-    this.positionX = this.positionX - this.velocity;
   }
 }
--- scripts/game/sprite-animation.js
+++ scripts/game/sprite-animation.js
@@ -5,12 +5,16 @@ class SpriteAnimation {
     positionX = 0,
     positionY,
     horizontalSpriteSize,
-    verticalSpriteSize
+    verticalSpriteSize,
+    velocity = 10,
+    direction = 'left'
   }) {
     this.spriteRatio = spriteRatio;
     this.imageSprite = imageSprite;
     this.positionX = positionX;
     this.positionY = positionY;
+    this.velocity = velocity;
+    this.direction = direction;
 
     this.spriteWidth = imageSprite.width / horizontalSpriteSize;
     this.spriteHeight = imageSprite.height / verticalSpriteSize;
@@ -53,5 +57,33 @@ class SpriteAnimation {
     });
 
     return spritePositions;
+  }
+
+  move() {
+    if (!this.velocity || !this.direction) {
+      console.error('Tentando mover um personagem sem velocidade ou direção informada.');
+    }
+
+    switch (this.direction) {
+      case 'left':
+        this.positionX = this.positionX - this.velocity;
+        break;
+
+      case 'right':
+        this.positionX = this.positionX + this.velocity;
+        break;
+
+      case 'top':
+        this.positionY = this.positionY - this.velocity;
+        break;
+
+      case 'bottom':
+        this.positionY = this.positionY + this.velocity;
+        break;
+    
+      default:
+        this.positionX = this.positionX - this.velocity;
+        break;
+    }
   }
 }
