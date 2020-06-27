3a6ef915d041f43c3ebd937d74c5625244620c15
Matheus Maghost <matheus@gamersclub.com.br>
2020-06-27 15:34:01 -0300

    feat: double jump and draw react (wip)

--- scripts/game/character.js
+++ scripts/game/character.js
@@ -6,6 +6,8 @@ class Character extends SpriteAnimation {
     this.gravity = gravity;
     this.jumpHeight = jumpHeight;
     this.jumpVelocity = 0;
+    this.jumps = 0;
+    this.maxJumps = 2;
 
     this.actionsByKey = {
       'ArrowUp': () => this.jump()
@@ -13,6 +15,9 @@ class Character extends SpriteAnimation {
   }
 
   jump() {
+    if (this.jumps >= this.maxJumps) return;
+
+    this.jumps++;
     this.jumpVelocity = -this.jumpHeight;
     this.jumpSound.play();
   }
@@ -23,17 +28,36 @@ class Character extends SpriteAnimation {
 
     if (this.positionY > this.initialY) {
       this.positionY = this.initialY;
+      this.jumps = 0;
     }
   }
 
   isColleded(enemy) {
     const collisionPrecision = 0.7;
 
+    this.renderCollision(character, enemy, collisionPrecision);
+
     return collideRectRect(
       this.positionX,
       this.positionY,
       this.spriteWidthDest * collisionPrecision,
       this.spriteHeightDest * collisionPrecision,
+      enemy.positionX,
+      enemy.positionY,
+      enemy.spriteWidthDest * collisionPrecision,
+      enemy.spriteHeightDest * collisionPrecision
+    );
+  }
+
+  renderCollision(character, enemy, collisionPrecision) {
+    noFill();
+    rect(
+      character.positionX,
+      character.positionY,
+      character.spriteWidthDest * collisionPrecision,
+      character.spriteHeightDest * collisionPrecision
+    );
+    rect(
       enemy.positionX,
       enemy.positionY,
       enemy.spriteWidthDest * collisionPrecision,
